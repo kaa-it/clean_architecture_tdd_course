7c69dda342630280d06d99578592b5d35a3db5db
Andrey Kruglov <kruglov.it@gmail.com>
2019-12-05 17:14:49 +0300

    Repository is completed

--- lib/features/number_trivia/data/models/number_trivia_model.dart
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -6,9 +6,9 @@ class NumberTriviaModel extends NumberTrivia {
     @required String text,
     @required int number,
   }) : super(
-    text: text,
-    number: number,
-  );
+          text: text,
+          number: number,
+        );
 
   factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
     return NumberTriviaModel(
